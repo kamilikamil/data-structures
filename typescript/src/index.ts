47a8fdbb706ff1c38e262ac2bbebf6f5b8f3e34b
Kamil Kamili <kamili.kamil@outlook.com>
2022-01-28 19:19:46 +0400

    feature added queue implementation

--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -1,7 +1,8 @@
 import { Stack } from "./structures/stack";
 import { SinglyLinkedList } from "./structures/linkedlist/single";
+import { Queue } from "./structures/queue";
 
-const stack = new Stack();
+const stack = new Stack<Number>();
 
 // stack.push(1);
 // stack.push(2);
@@ -19,18 +20,34 @@ const stack = new Stack();
 
 // console.dir(stack, {depth: Infinity});
 
-const list = new SinglyLinkedList();
+const list = new SinglyLinkedList<Number>();
 
-list.insert(5);
+// list.insert(5);
 
-list.insert(10);
+// list.insert(10);
 
-list.insert(20);
+// list.insert(20);
 
-list.insert(30);
+// list.insert(30);
 
-console.dir(list, { depth: Infinity });
+// console.dir(list, { depth: Infinity });
 
-list.remove(20);
+// list.remove(20);
 
-console.dir(list, { depth: Infinity });
+// console.dir(list, { depth: Infinity });
+
+const queue = new Queue<Number>();
+
+// queue.push(5);
+
+// queue.push(10);
+
+// queue.push(20);
+
+// queue.push(30);
+
+// console.dir(queue, { depth: Infinity });
+
+// queue.pop();
+
+// console.dir(queue, { depth: Infinity });
