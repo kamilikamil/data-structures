85b8db9f5a9b08b276524054ac785a51b2d8568d
Kamil Kamili <kamili.kamil@outlook.com>
2022-01-28 19:18:36 +0400

    feature: added queue implementation

---
+++ typescript/src/structures/queue/index.ts
@@ -0,0 +1,37 @@
+//Linked list will be a better option here to use for our queue rather than arrays
+//since queue is first in first out, we will have to always shift the values left by 1 in the array :(
+
+import { SinglyLinkedList } from "../linkedlist/single";
+
+export class Queue<T> {
+  private _queue: SinglyLinkedList<T> = new SinglyLinkedList();
+  private _headValue: T = null;
+
+  push(value: T) {
+    if (this._queue.get() === null) {
+      this._headValue = value;
+    }
+
+    this._queue.insert(value);
+  }
+
+  pop() {
+    if (this._headValue === null) {
+      return;
+    }
+
+    this._queue.remove(this._headValue);
+
+    if (this._queue.get() !== null) {
+      //@ts-ignore
+      this._headValue = this._queue.get().value;
+      return;
+    }
+
+    this._headValue = null;
+  }
+
+  peek(): T {
+    return this._headValue;
+  }
+}
