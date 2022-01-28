f9030e01c29ed87b00c177336ede83387a4c4e0c
Kamil Kamili <kamili.kamil@outlook.com>
2022-01-28 19:19:27 +0400

    feature: changed from Number to generic type

--- typescript/src/structures/linkedlist/single.ts
+++ typescript/src/structures/linkedlist/single.ts
@@ -1,17 +1,17 @@
 import { LinkedinList } from "./types";
 
-export class SinglyLinkedList {
-  private _list: LinkedinList | null = null; // lets make sure we don't mutate this variable directly
+export class SinglyLinkedList<T> {
+  private _list: LinkedinList<T> = null; // lets make sure we don't mutate this variable directly
 
-  private _mutateList(list: LinkedinList | null) {
+  private _mutateList(list: LinkedinList<T>) {
     this._list = list;
   }
 
-  get(): LinkedinList | null {
+  get(): LinkedinList<T> {
     return this._list;
   }
 
-  insert(value: Number) {
+  insert(value: T) {
     const linkedinList = new LinkedinList({
       value,
       next: null,
@@ -24,7 +24,7 @@ export class SinglyLinkedList {
       return;
     }
 
-    let tail: LinkedinList | null = this._list;
+    let tail: LinkedinList<T> = this._list; //this is a shallow copy (any changes being made to tail will be reflected in _list) :) :)
 
     while (tail.next != null) {
       tail = tail.next;
@@ -33,13 +33,13 @@ export class SinglyLinkedList {
     tail.next = linkedinList;
   }
 
-  exists(value: Number): boolean {
+  exists(value: T): boolean {
     // we have an empty linked list
     if (this._list === null) {
       return false;
     }
 
-    let temp: LinkedinList | null = this._list;
+    let temp: LinkedinList<T> = this._list;
     let found = false;
 
     while (temp !== null && !found) {
@@ -53,14 +53,18 @@ export class SinglyLinkedList {
     return found;
   }
 
-  remove(value: Number) {
+  remove(value: T) {
     // we have an empty linked list
     if (this._list === null) {
       return;
     }
 
-    let tail: LinkedinList | null = this._list;
-    let prev: LinkedinList | null = tail;
+    if (value === this._list.value) {
+      this._mutateList(this._list.next);
+    }
+
+    let tail: LinkedinList<T> = this._list; //this is a shallow copy (any changes being made to tail will be reflected in _list) :)
+    let prev: LinkedinList<T> = this._list; //this is a shallow copy (any changes being made to prev will be reflected in _list) :)
 
     while (tail !== null) {
       // we found the item to remove
